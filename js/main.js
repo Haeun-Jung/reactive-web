e7dfc98ba3dbc666ae98b5a1ea01067dbcc2e0d8
Haeun-Jung <gkdms7070@naver.com>
2021-03-04 23:42:29 +0900

    web 수정

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function moveBackToTop() {
 window.addEventListener("scroll", checkScroll);
 backToTop.addEventListener("click", moveBackToTop);
 
-// 강의 슬라이드 구현
+// 포토 슬라이드 구현
 
 // 오른쪽 버튼
 function transformNext(event) {
@@ -31,9 +31,9 @@ function transformNext(event) {
   // slide-next 클래스를 가진 요소 이전 요소는 slide-prev 클래스를 가진 요소
   const slidePrev = slideNext.previousElementSibling;
 
-  const classList = slideNext.parentElement.parentElement.nextElementSibling;
-  let activeLi = classList.getAttribute("data-position");
-  const liList = classList.getElementsByTagName("li");
+  const photoList = slideNext.parentElement.parentElement.nextElementSibling;
+  let activeLi = photoList.getAttribute("data-position");
+  const liList = photoList.getElementsByTagName("li");
 
   // 하나의 카드라도 왼쪽으로 이동했다면, 오른쪽으로 갈 수 있음
   if (Number(activeLi) < 0) {
@@ -52,9 +52,9 @@ function transformNext(event) {
     }
   }
 
-  classList.style.transition = "transform 1s";
-  classList.style.transform = "translateX(" + String(activeLi) + "px)";
-  classList.setAttribute("data-position", activeLi);
+  photoList.style.transition = "transform 1s";
+  photoList.style.transform = "translateX(" + String(activeLi) + "px)";
+  photoList.setAttribute("data-position", activeLi);
 }
 
 // 왼쪽 버튼
@@ -65,16 +65,16 @@ function transformPrev(event) {
   const slideNext = slidePrev.nextElementSibling;
 
   //ul 태그 선택
-  const classList = slidePrev.parentElement.parentElement.nextElementSibling;
-  let activeLi = classList.getAttribute("data-position");
-  const liList = classList.getElementsByTagName("li");
+  const photoList = slidePrev.parentElement.parentElement.nextElementSibling;
+  let activeLi = photoList.getAttribute("data-position");
+  const liList = photoList.getElementsByTagName("li");
 
-  if (classList.clientWidth < liList.length * 260 + Number(activeLi)) {
+  if (photoList.clientWidth < liList.length * 260 + Number(activeLi)) {
     // 위치를 왼쪽으로 260이동
     activeLi = Number(activeLi) - 260;
 
-    // 강의카드가 ul태그 너비보다 작으면 PREV버튼 비활성화
-    if (classList.clientWidth > liList.length * 260 + Number(activeLi)) {
+    // 포토카드가 ul태그 너비보다 작으면 PREV버튼 비활성화
+    if (photoList.clientWidth > liList.length * 260 + Number(activeLi)) {
       slidePrev.style.color = "#cfd8dc";
       slidePrev.classList.remove("slide-prev-hover");
       slidePrev.removeEventListener("click", transformPrev);
@@ -84,35 +84,35 @@ function transformPrev(event) {
     slideNext.classList.add("slide-next-hover");
     slideNext.addEventListener("click", transformNext);
   }
-  classList.style.transition = "transform 1s";
-  classList.style.transform = "translateX(" + String(activeLi) + "px)";
-  classList.setAttribute("data-position", activeLi);
+  photoList.style.transition = "transform 1s";
+  photoList.style.transform = "translateX(" + String(activeLi) + "px)";
+  photoList.setAttribute("data-position", activeLi);
 }
 
-// 강의카드와 화면 너비를 비교하여 버튼 출력
+// 포토카드와 화면 너비를 비교하여 버튼 출력
 const slidePrevList = document.getElementsByClassName("slide-prev");
 
 for (let i = 0; i < slidePrevList.length; i++) {
   // ul 태그 선택
-  let classList =
+  let photoList =
     slidePrevList[i].parentElement.parentElement.nextElementSibling;
-  let liList = classList.getElementsByTagName("li");
+  let liList = photoList.getElementsByTagName("li");
 
-  // 강의카드가 ul 태그 너비보다 넘치면, 왼쪽(prev)버튼은 활성화, 오른쪽(next)버튼은 현재 맨 첫 카드 위치이므로 비활성화
-  if (classList.clientWidth < liList.length * 260) {
+  // 포토카드가 ul 태그 너비보다 넘치면, 왼쪽(prev)버튼은 활성화, 오른쪽(next)버튼은 현재 맨 첫 카드 위치이므로 비활성화
+  if (photoList.clientWidth < liList.length * 260) {
     slidePrevList[i].classList.add("slide-prev-hover");
     slidePrevList[i].addEventListener("click", transformPrev);
   } else {
-    //강의카드가 ul 태그 너비보다 넘치지 않으면, PREV, NEXT 버튼 불필요하므로, 아예 삭제
+    //포토카드가 ul 태그 너비보다 넘치지 않으면, PREV, NEXT 버튼 불필요하므로, 아예 삭제
     const arrowContainer = slidePrevList[i].parentElement;
     arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
     arrowContainer.removeChild(slidePrevList[i]);
   }
 }
 
-// 강의카드 마우스효과 이벤트
+// 포토카드 마우스효과 이벤트
 let touchstartX;
-let currentClassList;
+let currentPhotoList;
 let currentImg;
 let currentActiveLi;
 let nowActiveLi;
@@ -121,7 +121,7 @@ let mouseStart;
 function processTouchMove(event) {
   // 해당 요소가 가진 고유동작 중단시키기
   event.preventDefault();
-  // currentActiveLi: class-list 에서 data-position 으로 현재 카드 위치를 알아냄
+  // currentActiveLi: photo-list 에서 data-position 으로 현재 카드 위치를 알아냄
   // touchstartX: 최초 요소의 x 좌표값
   // event.clientX: 드래그 중인 현재의 마우스 좌표값
   // event.touches[0].screenX : 터치의 좌표값
@@ -129,8 +129,8 @@ function processTouchMove(event) {
   nowActiveLi =
     Number(currentActiveLi) + (Number(currentX) - Number(touchstartX));
   //마우스 위치에 따라 즉시 카드 이동
-  currentClassList.style.transition = "transform 0s linear";
-  currentClassList.style.transform =
+  currentPhotoList.style.transition = "transform 0s linear";
+  currentPhotoList.style.transform =
     "translateX(" + String(nowActiveLi) + "px)";
 }
 
@@ -149,8 +149,8 @@ function processTouchStart(event) {
   currentImg.addEventListener("touchmove", processTouchMove);
   currentImg.addEventListener("touchend", processTouchEnd);
 
-  currentClassList = currentImg.parentElement.parentElement;
-  currentActiveLi = currentClassList.getAttribute("data-position");
+  currentPhotoList = currentImg.parentElement.parentElement;
+  currentActiveLi = currentPhotoList.getAttribute("data-position");
 }
 
 function processTouchEnd(event) {
@@ -167,17 +167,17 @@ function processTouchEnd(event) {
     currentImg.removeEventListener("touchend", processTouchEnd);
 
     // 맨 처음 카드가 맨 앞에 배치되도록 초기 상태로 이동
-    currentClassList.style.transition = "transform 1s ease";
-    currentClassList.style.transform = "translateX(0px)";
-    currentClassList.setAttribute("data-position", 0);
+    currentPhotoList.style.transition = "transform 1s ease";
+    currentPhotoList.style.transform = "translateX(0px)";
+    currentPhotoList.setAttribute("data-position", 0);
 
     // 맨 처음 카드가 맨 앞에 배치된 상태로 화살표 버튼도 초기 상태로 변경
     let eachSlidePrev =
-      currentClassList.previousElementSibling.children[1].children[0];
+      currentPhotoList.previousElementSibling.children[1].children[0];
     let eachSlideNext =
-      currentClassList.previousElementSibling.children[1].children[1];
-    let eachLiList = currentClassList.getElementsByTagName("li");
-    if (currentClassList.clientWidth < eachLiList.length * 260) {
+      currentPhotoList.previousElementSibling.children[1].children[1];
+    let eachLiList = currentPhotoList.getElementsByTagName("li");
+    if (currentPhotoList.clientWidth < eachLiList.length * 260) {
       eachSlidePrev.style.color = "#2f3059";
       eachSlidePrev.classList.add("slide-prev-hover");
       eachSlidePrev.addEventListener("click", transformPrev);
@@ -195,10 +195,10 @@ window.addEventListener("dragend", processTouchEnd);
 window.addEventListener("mouseup", processTouchEnd);
 
 // 인터페이스간의 오작동을 막기 위해, 카드 내의 이미지에만 드래그 인터페이스를 제공
-const classImgLists = document.querySelectorAll("ul li img");
-for (let i = 0; i < classImgLists.length; i++) {
+const photoImgLists = document.querySelectorAll("ul li img");
+for (let i = 0; i < photoImgLists.length; i++) {
   // 해당 요소에 마우스를 누르면 드래그를 시작할 수 있으므로 이벤트 걸기
-  classImgLists[i].addEventListener("mousedown", processTouchStart);
+  photoImgLists[i].addEventListener("mousedown", processTouchStart);
   // 터치 지원
-  classImgLists[i].addEventListener("touchstart", processTouchStart);
+  photoImgLists[i].addEventListener("touchstart", processTouchStart);
 }
